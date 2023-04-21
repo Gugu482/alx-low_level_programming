28a28917db2371f407e38f9721b3145005181a22
Gugu482 <sinegugumtshali@gmail.com>
2023-04-21 10:01:04 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include	<stdio.h>
+
+/**
+ * main	-	prints alphabet in lower case
+ * Return:	Always 0
+ */
+int main(void)
+
+{
+	char bd;
+
+	for (bd = 'a'; bd <= 'z'; bd++)
+	{
+		putchar(bd)
+	}
+	putchar('\n');
+	return (0);
